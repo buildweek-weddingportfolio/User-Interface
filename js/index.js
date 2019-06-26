97745118ce71bfc26220c0076f5d32646d35d35c
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-06-25 20:36:09 -0600

    Added  arrow button rotation on accordion in testimonials.less and index.js

--- js/index.js
+++ js/index.js
@@ -23,7 +23,7 @@ const navSlide = () => {
             }
         });
 
-        // hamburger animation
+        // hamburger animation (making the close symbol)
         toggler.classList.toggle('toggle');
 
     });
@@ -44,12 +44,14 @@ class Article {
         // console.log(this.expandButton)
 
         // Setting a click handler on the expandButton reference and calling the expandArticle method
-        this.expandButton.addEventListener('click' , () => this.expandArticle() )
+        this.expandButton.addEventListener('click' , () => this.expandArticle())
     }
     expandArticle() {
         //Using our reference to the article to toggle a class to expand or hide the article                                                                 
         this.article.classList.toggle('article-open');
         // console.log(`article is open!`)
+
+        this.expandButton.classList.toggle('rotate');
     }
 }
 
